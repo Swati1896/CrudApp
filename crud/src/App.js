fb784f5a6a59e4841f4b0d3820ad05514c3aa44b
Pankaj Bhakre <pankajbhakre11@gmail.com>
2024-04-29 14:55:04 +0530

    Crud App Function

---
+++ crud/src/App.js
@@ -0,0 +1,155 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import { employeeData } from './employeeData';
+
+function App() {
+
+  const [data, setData] = useState([]);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [age, setAge] = useState(0);
+  const [id, setId] = useState(0);
+  const [isUpdate, setIsUpdate] = useState(false);
+
+
+  useEffect(() => {
+    setData(employeeData)
+  }, []);
+
+  const handleEdit = (id) => {
+    const dt = data.filter(item => item.id === id);
+    if (dt !== undefined) {
+      setIsUpdate(true)
+      setId(id)
+      setFirstName(dt[0].firstName);
+      setLastName(dt[0].lastName);
+      setAge(dt[0].age);
+    }
+  }
+
+  const handleDelete = (id) => {
+    if (id > 0) {
+      if (window.confirm("Are you sure to delete this item ?")) {
+        const dt = data.filter(item => item.id !== id);
+        setData(dt);
+      }
+    }
+  }
+
+  const handleSave = (e) => {
+    let error = '';
+    if (firstName === '')
+      error += 'First name is required, ';
+
+    if (lastName === '')
+      error += 'Last name is required, ';
+
+    if (age <= 0)
+      error += 'Age is required.';
+
+    if (error === '') {
+
+
+      /* e.preventDefault(); */
+      const dt = [...data];
+      const newObject = {
+        id: employeeData.length + 1,
+        firstName: firstName,
+        lastName: lastName,
+        age: age
+      }
+      dt.push(newObject);
+      setData(dt);
+    }
+    else {
+      alert(error)
+    }
+  }
+
+  const handleUpdate = () => {
+    console.log("enter")
+    const index = data.map((item) => {
+      return item.id
+    }).indexOf(id);
+
+    const dt = [...data];
+    dt[index].firstName = firstName;
+    dt[index].lastName = lastName;
+    dt[index].age = age;
+    setData(dt);
+    handleClear();
+  }
+
+  const handleClear = () => {
+    setId(0)
+    setFirstName('');
+    setLastName('');
+    setAge('');
+    setIsUpdate(false);
+  }
+
+
+  return (
+    <div className="App">
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: "10px", marginBottom: "10px" }}>
+        <div>
+          <lable><b>First Name:</b>&nbsp;
+            <input type='text' placeholder='Enter First Name' onChange={(e) => setFirstName(e.target.value)} value={firstName} />
+          </lable>&nbsp;
+          <lable><b>Last Name:</b>&nbsp;
+            <input type='text' placeholder='Enter Last Name' onChange={(e) => setLastName(e.target.value)} value={lastName} />
+          </lable>&nbsp;
+          <lable><b>Age:</b>&nbsp;
+            <input type='text' placeholder='Enter Age' onChange={(e) => setAge(e.target.value)} value={age} />
+          </lable>&nbsp;
+        </div>
+
+        <div>
+          {
+            !isUpdate ?
+              <button className='btn btn-primary' onClick={() => handleSave()}>Add</button>
+              :
+              <button className='btn btn-primary' onClick={() => handleUpdate()}>Update</button>
+
+          }
+          <button className='btn btn-danger' onClick={() => handleClear()}>Clear</button>
+        </div>
+
+      </div>
+
+      <table className='table table-hover'>
+        <thead>
+          <tr>
+            <td><b>Sr No.</b></td>
+            <td><b>Id</b></td>
+            <td><b>First Name</b></td>
+            <td><b>Last Name</b></td>
+            <td><b>Age</b></td>
+            <td><b>Actions</b></td>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{item.id}</td>
+                  <td>{item.firstName}</td>
+                  <td>{item.lastName}</td>
+                  <td>{item.age}</td>
+                  <td>
+                    <button className='btn btn-primary' onClick={() => handleEdit(item.id)}>Edit</button>&nbsp;
+                    <button className='btn btn-danger' onClick={() => handleDelete(item.id)}>Delete</button>
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
---
+++ crud/src/employeeData.js
@@ -0,0 +1,20 @@
+export const employeeData = [
+    {
+        id: 1,
+        firstName: 'Swati',
+        lastName: 'Patil' ,
+        age: 28
+    },
+    {
+        id: 2,
+        firstName: 'Pankaj' ,
+        lastName: 'Bhakre',
+        age: 33
+    },
+    {
+        id: 3,
+        firstName: 'Aastha',
+        lastName: 'Bhakre',
+        age: 10
+    },
+]
